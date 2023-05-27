7deb466d02bc9e4a00ce820c848bfdc4a4b8a42f
PedroNascimento792 <pedrocapedrocas@gmail.com>
2023-05-27 16:22:35 -0300

    resolvido

---
+++ main.py
@@ -0,0 +1,15 @@
+num = int(input("Digite um número inteiro: "))
+if num <= 0:
+    print("Número inválido")
+elif num == 1:
+    print("Não primo")
+else:
+    is_prime = True
+    for i in range(2, num):
+        if num % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print("Primo")
+    else:
+        print("Não primo")
